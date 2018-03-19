a7bf2db14c8df7fa9e7c7f5ab3e94ff951ed1a96
Jordan M <jmcclusky17@gmail.com>
2018-03-19 18:26:50 -0500

    Finished class 9 "ViewModels and Validation".

--- src/CheeseMVC/Controllers/CheeseController.cs
+++ src/CheeseMVC/Controllers/CheeseController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using CheeseMVC.Models;
+using CheeseMVC.ViewModels;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -15,24 +16,39 @@ namespace CheeseMVC.Controllers
         // GET: /<controller>/
         public IActionResult Index()
         {
-            ViewBag.cheeses = CheeseData.GetAll();
+            List<Cheese> cheeses = CheeseData.GetAll();
 
-            return View();
+            return View(cheeses);
         }
 
         public IActionResult Add()
         {
-            return View();
+            AddCheeseViewModel addCheeseViewModel = new AddCheeseViewModel();
+            return View(addCheeseViewModel);
         }
 
         [HttpPost]
-        [Route("/Cheese/Add")]
-        public IActionResult NewCheese(Cheese newCheese)
+        //[Route("/Cheese/Add")]//just added to test if this fix
+        public IActionResult Add(AddCheeseViewModel addCheeseViewModel) //poplulates with the values from the form.
         {
-            // Add the new cheese to my existing cheeses
-            CheeseData.Add(newCheese);
-            
-            return Redirect("/Cheese");
+            if (ModelState.IsValid)// This will check that properties within the
+                //ViewModel that is being passed into the ActionMethod
+                //are valid according the the attributes that i've added,
+                //in particular the "Required" attribute will be enforced.
+            {
+                // Add the new cheese to my existing cheeses
+                Cheese newCheese = new Cheese
+                {
+                    Name = addCheeseViewModel.Name,
+                    Description = addCheeseViewModel.Description
+                };
+
+                CheeseData.Add(newCheese);
+
+                return Redirect("/Cheese");
+
+            }
+            return View(addCheeseViewModel);
         }
 
         public IActionResult Remove()
